ef5227f0453ab99c52893c00755a99327432b530
Tadeo <domenechtadeo@outlook.es>
2021-11-29 14:06:56 +0100

    Create the HomePage Component estructure

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,22 @@
+import ButtonHike from "../../components/ButtonHike/ButtonHike";
+import HeaderHike from "../../components/HeaderHike/HeaderHike";
+
 const HomePage = (): JSX.Element => {
-  return <h1>Soy Home Page</h1>;
+  const handlerButton = () => {
+    console.log("Hola");
+  };
+
+  return (
+    <>
+      <HeaderHike />
+      <div className="homepage__container">
+        <h4 className="homepage__description">
+          Welcome to the best walking routes, where you can enjoy nature.
+        </h4>
+        <ButtonHike text={"MAP"} click={handlerButton} />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
