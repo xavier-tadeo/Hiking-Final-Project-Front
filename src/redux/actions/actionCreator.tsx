ee47fc08e86187d0f5c7084e8ff94a8012392a1a
Tadeo <domenechtadeo@outlook.es>
2021-11-29 16:30:39 +0100

    Create action create for loginUser

--- src/redux/actions/actionCreator.tsx
+++ src/redux/actions/actionCreator.tsx
@@ -4,7 +4,16 @@ interface IHikes {
   hikes: Array<Object>;
 }
 
+interface IUser {
+  user: Object;
+}
+
 export const getHikesAction = ({ hikes }: IHikes) => ({
   type: actionTypes.getHikes,
   hikes,
+});
+
+export const loginUserAction = ({ user }: IUser) => ({
+  type: actionTypes.loginUser,
+  user,
 });
