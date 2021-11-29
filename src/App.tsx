d1a815cc8502a0013ff9848ec119724db2122960
Tadeo <domenechtadeo@outlook.es>
2021-11-29 22:45:27 +0100

    Create the LinkComponent added in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
+
 import "./App.css";
 import HeaderHike from "./components/HeaderHike/HeaderHike";
+import LinkComponent from "./components/LinkComponent/LinkComponent";
+import LoginForm from "./components/LoginForm/LoginForm";
+import HomePage from "./pages/HomePage/HomePage";
 
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import paths from "./paths/pathsHike";
@@ -11,11 +14,14 @@ function App(): JSX.Element {
     <>
       <HeaderHike />
       <BrowserRouter>
+        <LinkComponent />
         <Routes>
+          <Route path={paths.homePage} element={<HomePage />}></Route>
           <Route
             path={paths.registerFormPage}
             element={<RegisterPage />}
           ></Route>
+          <Route path={paths.loginFormPage} element={<LoginForm />}></Route>
         </Routes>
       </BrowserRouter>
     </>
