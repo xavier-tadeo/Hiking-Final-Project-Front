23ab413f6ab69df25980cf8caf6cff1677c66140
Tadeo <domenechtadeo@outlook.es>
2021-11-29 16:04:05 +0100

    Create the actions types for hike and user

---
+++ src/redux/actions/actionTypes.tsx
@@ -0,0 +1,10 @@
+const actionTypes = {
+  getHikes: "GET_HIKES",
+  postHike: "POST_HIKE",
+  updateHike: "UPDATE_HIKE",
+  getUsers: "GET_USERS",
+  registerUser: "REGISTER_USER",
+  loginUser: "LOGIN_USER",
+};
+
+export default actionTypes;
