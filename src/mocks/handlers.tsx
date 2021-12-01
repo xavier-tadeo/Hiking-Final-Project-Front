d2892b3912fcfaac95ceb3013eaca186734fc614
Tadeo <domenechtadeo@outlook.es>
2021-12-01 15:32:36 +0100

    Create the test for HomePage and create mock for homePage

--- src/mocks/handlers.tsx
+++ src/mocks/handlers.tsx
@@ -2,6 +2,34 @@ import { rest } from "msw";
 
 const mockToken = "1234567890";
 const mockId = "0987654321";
+const mockHikes = [
+  {
+    title: "Guinardo",
+    images: ["imagen1", "imagen2", "imagen3"],
+    stadistics: {
+      distance: "2km",
+      time: "3h",
+      dificulty: 3,
+      elevation: "200m",
+    },
+    user: {
+      name: "arlet",
+    },
+  },
+  {
+    title: "Montaña Rusa de emotions!",
+    images: ["imagen1", "imagen2", "imagen3"],
+    stadistics: {
+      distance: "2km",
+      time: "3h",
+      dificulty: 3,
+      elevation: "200m",
+    },
+    user: {
+      name: "arlet",
+    },
+  },
+];
 
 export const handlers = [
   rest.post(
@@ -15,6 +43,13 @@ export const handlers = [
     "https://proyecto-final-xavi-back.herokuapp.com/user/register",
     async (req, res, ctx) => {
       const respons = res(ctx.json(mockId));
+      return respons;
+    }
+  ),
+  rest.get(
+    "https://proyecto-final-cavi-back.herokuapp.com/hike/get",
+    async (req, res, ctx) => {
+      const respons = res(ctx.json(mockHikes));
       return respons;
     }
   ),
