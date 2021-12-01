9b8befbcbd98e4cc036a184bd1c71489067dd017
Tadeo <domenechtadeo@outlook.es>
2021-12-01 13:55:49 +0100

    Create the test for LoginPage

--- src/mocks/handlers.tsx
+++ src/mocks/handlers.tsx
@@ -4,7 +4,7 @@ const mockToken = "1234567890";
 
 export const handlers = [
   rest.post(
-    "https://proyecto-final-xavi-back.herokuapp.com/",
+    "https://proyecto-final-xavi-back.herokuapp.com/user/login",
     async (req, res, ctx) => {
       const respons = res(ctx.json(mockToken));
       return respons;
--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,15 +1,37 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { server } from "../../mocks/server";
+import configureStore from "../../redux/store";
 import LoginPage from "./LoginPage";
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
+const store = configureStore();
 
 describe("Given a LoginPage component", () => {
   describe("When it render", () => {
     test("It should rendered a one tah <h3> with name 'Login'", () => {
-      const h3Name = "Login";
+      const h3Name = "Name";
 
-      render(<LoginPage />);
-      const expectedTitle = screen.getByText(h3Name);
+      render(
+        <Provider store={store}>
+          <LoginPage />
+        </Provider>
+      );
+      const expectedTitle = screen.getByRole("heading", { name: "Login" });
+      const expectedPlaceholder = screen.getByPlaceholderText(h3Name);
 
       expect(expectedTitle).toBeInTheDocument();
+      expect(expectedPlaceholder).toBeInTheDocument();
     });
   });
 });
