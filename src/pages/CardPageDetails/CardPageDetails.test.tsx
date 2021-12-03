773ca2b65c3d2e63e05a0da4aaccb3c46ec7a3c2
Tadeo <domenechtadeo@outlook.es>
2021-12-03 10:28:33 +0100

    Create the test for CardPageDetails

---
+++ src/pages/CardPageDetails/CardPageDetails.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import configureStore from "../../redux/store";
+import CardPageDetails from "./CardPageDetails";
+
+const store = configureStore();
+
+describe("Given a CardPageDetails component", () => {
+  describe("When it render", () => {
+    test("It should rendered a card with title 'Details Hike'", () => {
+      const titlePage = "Details Hike";
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <CardPageDetails />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const expectedTitle = screen.getByRole("heading", {
+        name: titlePage,
+      });
+
+      expect(expectedTitle).toBeInTheDocument();
+    });
+  });
+});
