7f2396e65e2510b27ea83bd017500fc15c67d0af
Tadeo <domenechtadeo@outlook.es>
2021-12-05 00:41:51 +0100

    Fixed the route in thunk method getUser

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,25 +1,21 @@
-import jwtDecode from "jwt-decode";
-import { useEffect } from "react";
-
-import useUser from "../../hooks/useUser";
 import "./ProfilePage.scss";
 
 const ProfilePage = (): JSX.Element => {
-  const { user, getUser } = useUser();
+  // const { getUser } = useUser();
 
-  const tokenUser: any = localStorage.getItem("tokenStorage");
-  let token;
-  let idUser: string;
-  if (tokenUser) {
-    const userToken = JSON.parse(tokenUser);
-    token = userToken.token;
-    const tokenDecode: any = jwtDecode(token);
-    idUser = tokenDecode.id;
-  }
+  // const tokenUser: any = localStorage.getItem("tokenStorage");
+  // let token;
+  // let idUser: string;
+  // if (tokenUser) {
+  //   const userToken = JSON.parse(tokenUser);
+  //   token = userToken.token;
+  //   const tokenDecode: any = jwtDecode(token);
+  //   idUser = tokenDecode.id;
+  // }
 
-  useEffect(() => {
-    getUser(idUser);
-  }, [getUser]);
+  // useEffect(() => {
+  //   getUser(idUser);
+  // }, [getUser]);
   return (
     <div className="container__yourprofile">
       <h3 className="yourprofile__title">Your Profile</h3>
