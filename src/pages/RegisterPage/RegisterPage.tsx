e9f4404d8a974641c206fa112beb0e1504fe9591
Tadeo <domenechtadeo@outlook.es>
2021-12-04 01:01:02 +0100

    Added the backgroundimage in the register form

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -5,9 +5,11 @@ import "@fontsource/architects-daughter";
 const RegisterPage = (): JSX.Element => {
   return (
     <>
-      <h3 className="register__title">Register</h3>
-      <div className="register__form">
-        <RegisterForm />
+      <div className="container__registerform">
+        <h3 className="register__title">Register</h3>
+        <div className="register__form">
+          <RegisterForm />
+        </div>
       </div>
     </>
   );
