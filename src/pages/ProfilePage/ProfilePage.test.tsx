115b62b263e96329be8ec210754fd8dec268056d
Tadeo <domenechtadeo@outlook.es>
2021-12-04 22:28:34 +0100

    Added the image background ProfilePage

--- src/pages/ProfilePage/ProfilePage.test.tsx
+++ src/pages/ProfilePage/ProfilePage.test.tsx
@@ -4,11 +4,9 @@ import ProfilePage from "./ProfilePage";
 describe("Given a ProfilePage component", () => {
   describe("When render a component", () => {
     test("It should rendered a <h3> with title 'Construyendo MAPA'", () => {
-      const title = "Page Profile";
-
       render(<ProfilePage />);
       const expectTitle = screen.getByRole("heading", {
-        name: title,
+        name: "Your Profile",
       });
 
       expect(expectTitle).toBeInTheDocument();
