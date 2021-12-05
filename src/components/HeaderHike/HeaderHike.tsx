984ebd360d901fb2627dc46d269a13cdd8180aca
Tadeo <domenechtadeo@outlook.es>
2021-12-05 22:24:36 +0100

    Added the button Logout in the header

--- src/components/HeaderHike/HeaderHike.tsx
+++ src/components/HeaderHike/HeaderHike.tsx
@@ -4,7 +4,6 @@ const HeaderHike = (): JSX.Element => {
   return (
     <header className="container__header">
       <div className="header__icon">
-        
         <img
           src="hikingIcon.png"
           alt="logo hiking"
@@ -12,6 +11,7 @@ const HeaderHike = (): JSX.Element => {
         />
       </div>
       <h1 className="header__title">hiKing</h1>
+      <button className="button__logout">Logout</button>
     </header>
   );
 };
