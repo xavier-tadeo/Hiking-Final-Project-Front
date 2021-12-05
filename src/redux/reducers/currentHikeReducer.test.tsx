3c771ec106163d378ffa330778c117beca5ce9d0
Tadeo <domenechtadeo@outlook.es>
2021-12-05 13:37:32 +0100

    Create the test for currentHikeReducer action updateHike

--- src/redux/reducers/currentHikeReducer.test.tsx
+++ src/redux/reducers/currentHikeReducer.test.tsx
@@ -1,4 +1,3 @@
-import { IHike } from "../actions/actionCreator";
 import actionTypes from "../actions/actionTypes";
 import currentHikeReducer from "./currentHikeReducer";
 
@@ -39,6 +38,94 @@ describe("Given a currentHikeReducer", () => {
 
       const action = {
         type: actionTypes.currentHike,
+        hike: userTest,
+      };
+
+      const result = currentHikeReducer(initial, action);
+
+      expect(result).toEqual(userTest);
+    });
+  });
+
+  describe("When it receives a hike object and action postHike", () => {
+    test("Then it should return a new hike", () => {
+      const initial = {
+        title: "Guinardo",
+        images: ["imagen1", "imagen2", "imagen3"],
+        stadistics: {
+          distance: "2km",
+          time: "3h",
+          dificulty: 3,
+          elevation: "200m",
+        },
+        user: {
+          name: "arlet",
+        },
+      };
+
+      const userTest = {
+        hike: {
+          title: "Guinardo",
+          images: ["imagen1", "imagen2", "imagen3"],
+          stadistics: {
+            distance: "2km",
+            time: "3h",
+            dificulty: 3,
+            elevation: "200m",
+          },
+          user: {
+            name: "arlet",
+          },
+        },
+      };
+
+      const action = {
+        type: actionTypes.postHike,
+        hike: userTest,
+      };
+
+      const result = currentHikeReducer(initial, action);
+
+      expect(result).toEqual(userTest);
+    });
+  });
+
+  describe("When it receives a id with hike object and action updateHike", () => {
+    test("Then it should return a new hike", () => {
+      const initial = {
+        title: "Guinardo",
+        images: ["imagen1", "imagen2", "imagen3"],
+        stadistics: {
+          distance: "2km",
+          time: "3h",
+          dificulty: 3,
+          elevation: "200m",
+        },
+        user: {
+          name: "arlet",
+        },
+        id: "1234",
+      };
+
+      const userTest = {
+        hike: {
+          title: "Modificate",
+          images: ["imagen1", "imagen2", "imagen3"],
+          stadistics: {
+            distance: "10km",
+            time: "5h",
+            dificulty: 1,
+            elevation: "1100m",
+          },
+          user: {
+            name: "arlet",
+          },
+          id: "1234",
+        },
+      };
+
+      const action = {
+        type: actionTypes.updateHike,
         hike: userTest,
       };
 
