b9756c62193046885fbb50387d69b13ab27a73c1
Tadeo <domenechtadeo@outlook.es>
2021-12-06 17:17:10 +0100

    Added the mapCoordinates in MapPage

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,11 +1,25 @@
 import "./MapPage.scss";
 
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 const MapPage = (): JSX.Element => {
+  const coordinate = {
+    lat: 41.41993844003448,
+    lng: 2.181601662365733,
+  };
   return (
     <>
-      <div className="leaflet__container">
-        <h3 className="leaflet__map">MAP</h3>
-        <div className="leaflet"></div>
+      <div className="container">
+        <h3 className="title__map">MAP</h3>
+        <div className="container__map">
+          <MapContainer center={coordinate} zoom={12}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+            />
+          </MapContainer>
+        </div>
       </div>
     </>
   );
