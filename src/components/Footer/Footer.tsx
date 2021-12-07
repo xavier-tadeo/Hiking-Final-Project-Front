97c4b9e648790f9bd89e223ed60fa8329b30d1ee
Tadeo <domenechtadeo@outlook.es>
2021-12-07 11:46:21 +0100

    Create the component Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="container__footer">
+      <p className="footer__info">Xavier Tadeo ISDICoders 2021</p>
+    </div>
+  );
+};
+
+export default Footer;
