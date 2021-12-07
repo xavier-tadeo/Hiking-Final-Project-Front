36c2929c25bc4d35b3f8e10915cc93ed6daf250f
Tadeo <domenechtadeo@outlook.es>
2021-12-07 20:45:38 +0100

    Create the file test and pased test render CardPageDetails

---
+++ src/pages/CardPageDetails/CardPageDetails.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import configureStore from "../../redux/store";
+import CardPageDetails from "./CardPageDetails";
+
+const store = configureStore();
+describe("Given a HeaderHike component", () => {
+  describe("When it render", () => {
+    test("It should render h1 with name 'hiking' and one image", () => {
+      const title = "Details Hike";
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <CardPageDetails />
+          </BrowserRouter>
+        </Provider>
+      );
+      const titleh3 = screen.getByText(title);
+
+      expect(titleh3).toBeInTheDocument();
+    });
+  });
+});
