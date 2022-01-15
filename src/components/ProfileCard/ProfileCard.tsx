940363fb0a67586b4e670b9b847a8cacab944a51
Tadeo <domenechtadeo@outlook.es>
2022-01-15 17:59:29 +0100

    Remove part code ProfilePage create component ProfileCard added it

---
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const ProfileCard = ({ user }: any) => {
+  return (
+    <div className="yourprofile__container-info">
+      <h6 className="yourprofile__name info title">NAME</h6>
+      <p className="Yourprofile__name-user info user">{user.name}</p>
+      <h6 className="yourprofile__email info title">Email</h6>
+      <p className="Yourprofile__email-user info user">{user.email}</p>
+      <h6 className="yourprofile__routes info title">Your Routes</h6>
+      <p className="Yourprofile__routes-user info user">{user.yourRoutes}</p>
+      <h6 className="yourprofile__favorite info title">Your Favorite Routes</h6>
+      <p className="Yourprofile__favorite-user info user">{user.favorite}</p>
+    </div>
+  );
+};
--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { ProfileCard } from "../../components/ProfileCard/ProfileCard";
 import { lookLocalStorage } from "../../helpers/lookLocalStorage";
 import useUser from "../../hooks/useUser";
 import "./ProfilePage.scss";
@@ -15,18 +16,7 @@ const ProfilePage = (): JSX.Element => {
   return (
     <div className="container__yourprofile">
       <h3 className="yourprofile__title">Your Profile</h3>
-      <div className="yourprofile__container-info">
-        <h6 className="yourprofile__name info title">NAME</h6>
-        <p className="Yourprofile__name-user info user">{user.name}</p>
-        <h6 className="yourprofile__email info title">Email</h6>
-        <p className="Yourprofile__email-user info user">{user.email}</p>
-        <h6 className="yourprofile__routes info title">Your Routes</h6>
-        <p className="Yourprofile__routes-user info user">{user.yourRoutes}</p>
-        <h6 className="yourprofile__favorite info title">
-          Your Favorite Routes
-        </h6>
-        <p className="Yourprofile__favorite-user info user">{user.favorite}</p>
-      </div>
+      <ProfileCard user={user} />
     </div>
   );
 };
