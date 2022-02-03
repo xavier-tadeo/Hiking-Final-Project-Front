5f30a734f2ed4004990b42572fd8c133425c63c6
Tadeo <domenechtadeo@outlook.es>
2022-02-03 23:37:47 +0100

    Create new router with all routes less initialPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import HeaderHike from "./components/HeaderHike/HeaderHike";
-import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import pathsHike from "./paths/pathsHike";
-import { Toaster } from "react-hot-toast";
-import CardPageDetails from "./pages/CardPageDetails/CardPageDetails";
-import CreateHikePage from "./pages/CreateHikePage/CreateHikePage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import Footer from "./components/Footer/Footer";
 import { AppRouter } from "./routers/AppRouter";
 
 function App(): JSX.Element {
-  return (
-    <>
-      {/* <Toaster position="bottom-center" reverseOrder={false} />
-      <BrowserRouter>
-        <HeaderHike />
-        <Routes>
-          <Route path={pathsHike.bestRoutes} element={<HomePage />}></Route>
-          <Route
-            path={pathsHike.registerFormPage}
-            element={<RegisterPage />}
-          ></Route>
-          <Route path={pathsHike.loginFormPage} element={<LoginPage />}></Route>
-
-          <Route
-            path={pathsHike.hikeCard}
-            element={<CardPageDetails />}
-          ></Route>
-          <Route
-            path={pathsHike.createHike}
-            element={<CreateHikePage />}
-          ></Route>
-          <Route path={pathsHike.userProfile} element={<ProfilePage />}></Route>
-        </Routes>
-        <Footer />
-      </BrowserRouter> */}
-      <AppRouter />
-    </>
-  );
+  return <AppRouter />;
 }
 
 export default App;
--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HeaderHike from "../components/HeaderHike/HeaderHike";
+
+import { DashboardRoutes } from "./DashboardRoutes";
 import pathsHike from "../paths/pathsHike";
 
 export const AppRouter = () => {
@@ -8,6 +10,7 @@ export const AppRouter = () => {
       <BrowserRouter>
         <Routes>
           <Route path={pathsHike.headerPage} element={<HeaderHike />} />
+          <Route path="/*" element={<DashboardRoutes />} />
         </Routes>
       </BrowserRouter>
     </div>
