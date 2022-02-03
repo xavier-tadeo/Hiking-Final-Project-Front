aace219c9134e6a63ac42b06557676b73ae631aa
Tadeo <domenechtadeo@outlook.es>
2022-02-04 00:16:47 +0100

    Added link register and login in InintialPage

---
+++ src/pages/InitialPage/InitialPage.tsx
@@ -0,0 +1,11 @@
+import Footer from "../../components/Footer/Footer";
+import HeaderHike from "../../components/HeaderHike/HeaderHike";
+
+export const InitialPage = () => {
+  return (
+    <div>
+      <HeaderHike />
+      <Footer />
+    </div>
+  );
+};
--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HeaderHike from "../components/HeaderHike/HeaderHike";
 
 import { DashboardRoutes } from "./DashboardRoutes";
 import pathsHike from "../paths/pathsHike";
+import { InitialPage } from "../pages/InitialPage/InitialPage";
 
 export const AppRouter = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path={pathsHike.headerPage} element={<HeaderHike />} />
+          <Route path={pathsHike.headerPage} element={<InitialPage />} />
           <Route path="/*" element={<DashboardRoutes />} />
         </Routes>
       </BrowserRouter>
